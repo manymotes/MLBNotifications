3ab7c610109ad9cbf974e2bf511817aa9945c93c
kendall motes <kmotes@solutionreach.com>
2019-04-11 22:56:23 -0600

    got javascript to load

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(3000);
