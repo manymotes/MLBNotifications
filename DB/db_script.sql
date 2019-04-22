96c6f5c6413feaa0ba3d8ed24cf3a17177f7b078
kendall motes <kmotes@solutionreach.com>
2019-04-22 15:59:10 -0600

    added pitchers table to db script

---
+++ DB/db_script.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  f_name VARCHAR(40) NOT NULL,
+  l_name VARCHAR(25) NOT NULL,
+  password varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  date_created DATE NOT NULL
+);
+
+CREATE TABLE pitchers (
+  id INT,
+  name VARCHAR(150) NOT NULL,
+  FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- INSERT into pitchers (id, name) value (1, "Trevor Bauer");
--- DB/user_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-id INT AUTO_INCREMENT,
-f_name VARCHAR(40) NOT NULL,
-l_name VARCHAR(25) NOT NULL,
-password varchar(100) NOT NULL,
-email varchar(100) NOT NULL,
-date_created DATE NOT NULL
-);
